67279bf56d5e9fbbbec55e5f2e01d93494742fc4
hamzafoy <hamzaoflouisville@gmail.com>
2022-07-06 18:43:04 -0400

    importing Bootstrap Clean-Blog .css file and installed Nodemon.

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 // :::: Modules Importing
-import fs from 'fs';
-import http from 'http';
+import express from 'express'; //importing Express.js
+import path from 'path'; //importing path module to retrieve paths across various OS
+import { fileURLToPath } from 'url'; //Alternative to CommonJS's __dirname usage
+import { dirname } from 'path'; //Alternative to CommonJS's __dirname usage
+const __filename = fileURLToPath(import.meta.url); //Alternative to CommonJS's __dirname usage
+const __dirname = dirname(__filename); //Alternative to CommonJS's __dirname usageC
 
 
-// :::: Files Importing
-const homePage = fs.readFileSync('index.html');
-const aboutPage = fs.readFileSync('about.html');
-const contactPage = fs.readFileSync('contact.html');
-const fourOhFour = fs.readFileSync('404.html');
+// :::: Starting new Express application
+const application = express();
+application.use(express.static('public')); //instructs Express to serve assets from a 'public' directory
 
 
-// :::: Creating Server
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    switch(req.url) {
-        case '/':
-            res.end(homePage);
-            break;
-        case '/about':
-            res.end(aboutPage);
-            break;
-        case '/contact':
-            res.end(contactPage);
-            break;
-        default:
-            res.writeHead(404);
-            res.end(fourOhFour);
-    }
-});
+// :::: Creating Routes
+application.get('/', (req, res) => { //root route
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+})
 
-server.listen(3000);
+application.get('/about', (req, res) => { //about route
+    res.sendFile(path.resolve(__dirname, 'about.html'));
+})
+
+application.get('/contact', (req, res) => { //contact route
+    res.sendFile(path.resolve(__dirname, 'contact.html'));
+})
+
+application.listen(3000, () => {
+    console.log("We are tuned in to Localhost 3000");
+})
