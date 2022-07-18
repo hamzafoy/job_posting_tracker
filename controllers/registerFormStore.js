b0ffd6d805e9f0817f4f2f7d598551b32d2a212e
hamzafoy <hamzaoflouisville@gmail.com>
2022-07-18 15:10:38 -0400

    imported bcrypt and utilized its library to handle password encryption.

--- controllers/registerFormStore.js
+++ controllers/registerFormStore.js
@@ -1,7 +1,11 @@
 import User from "../models/User.js"; //importing the Database model for use in POST requests
 
 const registerFormStore = async(req, res) => {
-    await User.create(req.body);
+    await User.create(req.body, (error, user) => {
+        if(error) {
+            return res.redirect('/auth/register');
+        }
+    });
     res.redirect('/');
 };
 
--- models/User.js
+++ models/User.js
@@ -3,11 +3,32 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 
+// :::: Importing password encryption tool
+import bcrypt from 'bcrypt';
+
+
 // :::: Scaffolding User schema
 const UserSchema = new Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
+
+
+// :::: Encrypt user's chosen password before saving it in database
+UserSchema.pre('save', function(next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash;
+        next();
+    })
+})
 
 
 // :::: Accessing database through model
