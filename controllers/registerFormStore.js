e0b3c4cc830d3014089c0162bb1f9525b8da4649
hamzafoy <hamzaoflouisville@gmail.com>
2022-07-21 16:15:15 -0400

    paring error messages in console.

--- controllers/registerFormStore.js
+++ controllers/registerFormStore.js
@@ -3,6 +3,8 @@ import User from "../models/User.js"; //importing the Database model for use in 
 const registerFormStore = async(req, res) => {
     await User.create(req.body, (error, user) => {
         if(error) {
+            const errorMessage = Object.keys(error.errors).map(key => error.errors[key].message);
+            console.log(errorMessage);
             return res.redirect('/auth/register');
         }
     });
