d824fc899f6e56f950297d44a7290efbf08a3e03
hamzafoy <hamzaoflouisville@gmail.com>
2022-07-25 18:41:15 -0400

    enabled error messages to be disappeared upon reload of page.

--- controllers/registerForm.js
+++ controllers/registerForm.js
@@ -1,7 +1,9 @@
 const registerForm = async(req, res) => {
     res.render('register', {
-        errors: req.session.validationErrors
+        errors: req.session.validationErrors,
     });
+    req.session.validationErrors = '';
+    req.session.save();
 };
 
 export default registerForm;
